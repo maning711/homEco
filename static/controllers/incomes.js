4fe8eb19f3476a4b45470dbb092208553549b6cf
277943578@qq.com <mama:711117>
2016-04-07 17:33:27 +0800

    add the calendar Signed-off-by: 277943578@qq.com <mama:711117>

--- static/controllers/incomes.js
+++ static/controllers/incomes.js
@@ -2,5 +2,10 @@
  * created by maning
  */
 angular.module('homEco').controller('IncomesCtrl', function($scope, $http, $location){
-    
+    $scope.select = function (string) {
+        $scope.typeIncome = string;
+    };
+    $scope.deleteDate = function () {
+        $scope.selectedDate = '';
+    };
 });
